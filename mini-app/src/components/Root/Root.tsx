de5b1d99a2ae5e7f8e2b4da5a0e6d11ab96db01d
Alexander <iamkoldun@cuboserver.net>
2025-06-10 23:16:26 +0400

    FRONT-1: unnesessary files removed

--- mini-app/src/components/Root/Root.tsx
+++ mini-app/src/components/Root/Root.tsx
@@ -1,8 +1,7 @@
 'use client';
 
-import { type PropsWithChildren, useEffect } from 'react';
+import { type PropsWithChildren } from 'react';
 import {
-  initData,
   miniApp,
   useLaunchParams,
   useSignal,
@@ -13,7 +12,6 @@ import { AppRoot } from '@telegram-apps/telegram-ui';
 import { ErrorBoundary } from '@/components/ErrorBoundary';
 import { ErrorPage } from '@/components/ErrorPage';
 import { useDidMount } from '@/hooks/useDidMount';
-import { setLocale } from '@/core/i18n/locale';
 
 import './styles.css';
 
@@ -21,12 +19,6 @@ function RootInner({ children }: PropsWithChildren) {
   const lp = useLaunchParams();
 
   const isDark = useSignal(miniApp.isDark);
-  const initDataUser = useSignal(initData.user);
-
-  // Set the user locale.
-  useEffect(() => {
-    initDataUser && setLocale(initDataUser.language_code);
-  }, [initDataUser]);
 
   return (
     <TonConnectUIProvider manifestUrl="/tonconnect-manifest.json">
