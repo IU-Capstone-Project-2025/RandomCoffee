59cc7d65930abb1cfb19eaecb93e83ab018c5514
almax07082005 <almaxim07082005@gmail.com>
2025-06-17 17:35:52 +0300

    BACK-4: setup openAPI generator

---
+++ backend/API-gateway/src/main/java/iu/apigateway/controller/DefaultController.java
@@ -0,0 +1,8 @@
+package iu.apigateway.controller;
+
+import iu.apigateway.api.DefaultApi;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class DefaultController implements DefaultApi {
+}
