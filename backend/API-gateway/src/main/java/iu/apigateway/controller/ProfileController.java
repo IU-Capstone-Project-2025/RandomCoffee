a2f5218467911b46dee699a9193aa7b7366368ce
almax07082005 <almaxim07082005@gmail.com>
2025-06-25 18:25:19 +0300

    BACK-7: add API-gateway routing

--- backend/API-gateway/src/main/java/iu/apigateway/controller/ProfileController.java
+++
@@ -1,8 +0,0 @@
-package iu.apigateway.controller;
-
-import iu.apigateway.api.ProfileApi;
-import org.springframework.web.bind.annotation.RestController;
-
-@RestController
-public class ProfileController implements ProfileApi {
-}
